73d43900311850ea3a0bf883735f583e5a3afb77
francesco-strazzullo <me@francescostrazzullo.info>
2015-01-25 17:23:21 +0100

    toggle esample page

---
+++ src/main/java/org/primefaces/material/showcase/components/ToggleBean.java
@@ -0,0 +1,24 @@
+package org.primefaces.material.showcase.components;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class ToggleBean implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private boolean value;
+
+	public boolean isValue() {
+		return value;
+	}
+
+	public void setValue(boolean value) throws InterruptedException {
+		this.value = value;
+	}
+
+}
