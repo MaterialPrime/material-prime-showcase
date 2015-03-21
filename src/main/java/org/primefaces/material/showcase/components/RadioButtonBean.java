56dd7c9c58f51a629eb4b271d7a1d8ad24b9279b
luca.cardinali <lucacard87@gmail.com>
2015-03-21 10:22:38 +0100

    radioButton and selectOne

---
+++ src/main/java/org/primefaces/material/showcase/components/RadioButtonBean.java
@@ -0,0 +1,51 @@
+package org.primefaces.material.showcase.components;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.model.SelectItem;
+
+@ManagedBean
+@ViewScoped
+public class RadioButtonBean implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private String valueFixed = "1";
+	private Object valueDynamically;
+	private List<SelectItem> list;
+	
+	public RadioButtonBean() {
+		list = new ArrayList<SelectItem>();
+		list.add(new SelectItem("1", "Option 1"));
+		list.add(new SelectItem("2", "Option 2"));
+		list.add(new SelectItem("3", "Option 3")); 
+	}
+	
+	public String getValueFixed() {
+		return valueFixed;
+	}
+	
+	public void setValueFixed(String valueFixed) {
+		this.valueFixed = valueFixed;
+	}
+	
+	public Object getValueDynamically() {
+		return valueDynamically;
+	}
+	
+	public void setValueDynamically(Object valueDynamically) {
+		this.valueDynamically = valueDynamically;
+	}
+
+	public List<SelectItem> getList() {
+		return list;
+	}
+
+	public void setList(List<SelectItem> list) {
+		this.list = list;
+	}
+}
